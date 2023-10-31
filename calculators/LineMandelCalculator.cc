acac44a3b96abbc673dae34daf0b528e40bbd92f
David Chocholaty <david.chocholaty12@gmail.com>
2023-10-31 12:46:50 +0100

    LineMandelCalculator: add new version
    This commit adds the new implementation version for the
    LineMandelCalculator.

--- calculators/LineMandelCalculator.cc
+++ calculators/LineMandelCalculator.cc
@@ -18,51 +18,90 @@
 LineMandelCalculator::LineMandelCalculator (unsigned matrixBaseSize, unsigned limit) :
 	BaseMandelCalculator(matrixBaseSize, limit, "LineMandelCalculator")
 {
-    data = (int *)(malloc(height * width * sizeof(int)));
-    real_storage = (float *)(malloc(width * sizeof(float)));
-    imag_storage = (float *)(malloc(width * sizeof(float)));
+/*
+    data  = (int *)(aligned_alloc(64, height * width * sizeof(int)));
+    real_storage = (float *)(aligned_alloc(64, width * sizeof(float)));
+    imag_storage = (float *)(aligned_alloc(64, width * sizeof(float)));
+*/
+    data  = (int *)(_mm_malloc(height * width * sizeof(int), 64));
+    real_storage = (float *)(_mm_malloc(width * sizeof(float), 64));
+    imag_storage = (float *)(_mm_malloc(width * sizeof(float), 64));
 }
 
 LineMandelCalculator::~LineMandelCalculator() {
+/*
     free(data);
     data = NULL;
 
-    free(real_storage);
-    real_storage = NULL;
-
     free(imag_storage);
     imag_storage = NULL;
+
+    free(real_storage);
+    real_storage = NULL;
+*/
+    _mm_free(data);
+    data = NULL;
+
+    _mm_free(imag_storage);
+    imag_storage = NULL;
+
+    _mm_free(real_storage);
+    real_storage = NULL;
 }
 
 
 int * LineMandelCalculator::calculateMandelbrot () {
-    // TODO matice je symetricka, vypocet jen poloviny
-    for (int i = 0; i < height * width; i++) {
-        data[i] = 0;
+    const int half_height = height / 2;
+
+    // TODO otestovat jestli ponechat safelen
+    #pragma omp simd simdlen(64) safelen(64)
+    for (int i = 0; i <= half_height * width; i++) {
+        data[i] = limit;
     }
 
-    for (int i = 0; i < height; i++) {
-        float y = y_start + i * dy; // current imaginary value
+    for (int i = 0; i <= half_height; i++) {
+        const int row_start = i * width;
+
+        const float y = y_start + i * dy; // current imaginary value
+
+        #pragma omp simd simdlen(64) safelen(64)
+        for (int j = 0; j < width; j++) {
+            real_storage[j] = x_start + j * dx; // current real value
+            imag_storage[j] = y;
+        }
+
+        int count = width;
 
         for (int k = 0; k < limit; k++) {
+
+            #pragma omp simd reduction(-: count) simdlen(64) safelen(64)
             for (int j = 0; j < width; j++) {
-                float x = x_start + j * dx; // current real value
+                if (data[row_start + j] == limit) {
+                    const float r2 = real_storage[j] * real_storage[j];
+                    const float i2 = imag_storage[j] * imag_storage[j];
 
-                float zReal = (k == 0) ? x : real_storage[j];
-                float zImag = (k == 0) ? y : imag_storage[j];
-
-                float r2 = zReal * zReal;
-                float i2 = zImag * zImag;
-
-
-                if (r2 + i2 <= 4.0f) {
-                    real_storage[j] = r2 - i2 + x;
-                    imag_storage[j] = 2.0f * zReal * zImag + y;
-                    data[i * width + j] += 1;
+                    if (r2 + i2 > 4.0f) {
+                        data[row_start + j] = k;
+                        --count;
+                    } else {
+                        imag_storage[j] = 2.0f * real_storage[j] * imag_storage[j] + y;
+                        real_storage[j] = r2 - i2 + x_start + j * dx;
+                    }
                 }
             }
+
+            if (count == 0) {
+                break;
+            }
+        }
+
+        const int copy_row_start = (height - i - 1) * width;
+
+        #pragma omp simd simdlen(64) safelen(64)
+        for (int j = 0; j < width; j++) {
+            data[copy_row_start + j] = data[row_start + j];
         }
     }
 
-	return data;
+    return data;
 }
